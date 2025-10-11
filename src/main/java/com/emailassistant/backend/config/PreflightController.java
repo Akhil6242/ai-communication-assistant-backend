6ada19dbbf3fad0f03249995cf5d820d91a4c88c
Akhilesh Yadav <akhilyadavbil@gmail.com>
2025-10-11 22:24:25 +0530

    Modifying CORS Config policies to fetch fresh data

---
+++ src/main/java/com/emailassistant/backend/config/PreflightController.java
@@ -0,0 +1,15 @@
+package com.emailassistant.backend.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PreflightController {
+
+    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
+    public ResponseEntity<?> handle() {
+        return ResponseEntity.ok().build();
+    }
+}
